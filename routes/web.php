e5aef982b4b9c9bf541ed17ccf0e200029929936
AbdallahSaberAbuElazm <55307246+AbdallahSaberAbuElazm@users.noreply.github.com>
2022-02-16 14:58:11 +0200

    file upload

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('file-upload', [FileController::class, 'index'])->name('file-upload');
+Route::post('store', [FileController::class, 'store'])->name('store');
