dd4b2d69c4befc8816a4593af0b4a13dd7a1b7b4
AbdallahSaberAbuElazm <55307246+AbdallahSaberAbuElazm@users.noreply.github.com>
2022-02-19 16:38:53 +0200

    update registeration form

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -64,10 +65,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+        $user = new User();
+        $user->first_name    = $data['first_name'];
+        $user->last_name     = $data['last_name'];
+        $user->email         = $data['email'];
+        $user->password      = Hash::make($data['password']);
+        $user->api_token     = bin2hex(openssl_random_pseudo_bytes(30));
+        $user->save() ;
+        return $user;
     }
 }
