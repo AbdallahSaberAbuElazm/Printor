f5b2f1bd62b1307a399abe49cad3c01326f3b2d8
AbdallahSaberAbuElazm <55307246+AbdallahSaberAbuElazm@users.noreply.github.com>
2022-02-20 11:07:35 +0200

    update voucher table

--- database/factories/LibraryOwnerFactory.php
+++ database/factories/LibraryOwnerFactory.php
@@ -16,6 +16,10 @@ class LibraryOwnerFactory extends Factory
     {
         return [
 
+            'start_at'      => $this->faker->dateTime(),
+            'expires_at'    =>$this->faker->dateTime(),
+            'available'     =>$this->faker->numberBetween(0,1),
+            'rating'        =>$this->faker->numberBetween(1,5),
             'option_id'     =>$this->faker->numberBetween(1,3),
         ];
     }
--- database/migrations/2022_02_19_145813_create_vouchers_table.php
+++ database/migrations/2022_02_19_145813_create_vouchers_table.php
@@ -43,10 +43,10 @@ class CreateVouchersTable extends Migration
             $table->boolean('is_fixed')->default(true);
 
             // When the voucher begins
-            $table->timestamp('starts_at');
+            $table->timestamp('starts_at')->nullable();
 
             // When the voucher ends
-            $table->timestamp('expires_at');
+            $table->timestamp('expires_at')->nullable();
 
             // You know what this is...
             $table->timestamps();
