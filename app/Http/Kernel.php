74b1c0c3e90cba0f121e338d54efa0a440ca1bde
AbdallahSaberAbuElazm <55307246+AbdallahSaberAbuElazm@users.noreply.github.com>
2022-02-21 01:06:07 +0200

    add authentication access

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'email_verified'=>\App\Http\Middleware\EmailVerified::class,
+        'user_is_admin'=> \App\Http\Middleware\UserIsAdmin::class,
+        'user_is_support'=>\App\Http\Middleware\UserIsLibraryOwner::class,
     ];
 }
